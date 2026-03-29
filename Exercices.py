c65463a5f2622a097dc628937d69a0c1b099ea62
thesilvadev <nicolashenriquesilva@gmail.com>
2026-03-29 17:46:02 -0300

    Update Exercices.py

--- Exercices.py
+++ Exercices.py
@@ -161,31 +161,51 @@
 #Login com Níveis: Peça o login. Se for "admin", peça a senha. Se a senha
 #estiver correta, pergunte se quer "Reiniciar Sistema" ou "Desligar". Se o login
 #não for "admin", diga "Acesso de Usuário Comum"
-print("\n----------LOGIN----------\n")
+
+while True:
+    print("\n----------LOGIN----------\n")
     
-sen2= "170106"
-user= "admin"
-senha= "170106"
-
-login= input("Nome de Usuário: ").lower().strip()
+    user= "admin"
+    senha= "170106"
 
 
-if login != user:
-    print("Acesso de Usuário Comum")
+    login= input("\nNome de Usuário: ").lower().strip()
 
-else:
-    sen1= input("\nDigite a senha: ")
-    if sen1 == senha:
-        print("\nACESSO DE ADMINISTRADOR CONCEDIDA")
+
+    if login != user:
+        user_c= str(input("\nDigite a senha: "))
+        print("\nAcesso de Usuário Comum Concedida")
+
+        reiniciar= input("\nDeseja reiniciar o sistema? (s/n)\nR: ")
+        if reiniciar == "n":
+            print("FIM DO PROGRAMA")
+            break
+
+
     else:
-        while True:   
-            ask2= input("\nSenha Incorreta! Tentar novamente? (s/n)\nR: ")
-
-            if ask2 == "n":
+        sen1= input("\nDigite a senha: ")
+        if sen1 == senha:
+            print("\nACESSO DE ADMINISTRADOR CONCEDIDA")
+            reiniciar= input("\nDeseja reiniciar o sistema? (s/n)\nR: ")
+            if reiniciar == "n":
                 print("FIM DO PROGRAMA")
                 break
-            elif ask2 == "s":
-                sen2= input("\nDigite a senha: ")
-                if sen2 == senha:
-                    print("\nACESSO DE ADMINISTRADOR CONCEDIDA")
+        else:   
+            ask2= input("\nSenha Incorreta! Tentar novamente? (s/n)\nR: ")
+            if ask2 == "n":
+                print()
+            while True:
+
+                if ask2 == "n":
+                    print("FIM DO PROGRAMA")
                     break
+                elif ask2 == "s":
+                    sen2= input("\nDigite a senha: ")
+                    if sen2 == senha:
+                        print("\nACESSO DE ADMINISTRADOR CONCEDIDA\n")
+                        reiniciar= input("\nDeseja reiniciar o sistema? (s/n)\nR: \n")
+                        if reiniciar == "n":
+                            break
+                    else:
+                        ask2= input("\nSenha Incorreta! Tentar novamente? (s/n)\nR: ")
+                        
