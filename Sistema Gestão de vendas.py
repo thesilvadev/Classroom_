132be1c955844ccd85152b728f5212d798bda829
Nicolas <nicolashenriquesilva@gmail.com>
2026-04-15 00:17:07 -0300

    estrutura de soma

--- Sistema Gestão de vendas.py
+++ Sistema Gestão de vendas.py
@@ -3,18 +3,23 @@ print("\n------SISTEMA DE GESTÃO DE VENDAS MENSAIS------\n")
 
 nome_gestor = input("\nDigite o nome do gestor: ").capitalize()
 
-mes_referencia = input("\nDigite o mês de referência: ").upper()
+mes = input("\nDigite o mês de referência: ").upper()
 
-while mes_referencia != "JANEIRO" and "FEVEREIRO" and "MARÇO":#mes_referencia #!= "FEVEREIRO" and mes_referencia != "MARÇO" and mes_referencia != "ABRIL" and mes_referencia !=  "MAIO" and mes_referencia != :
+while mes != "JANEIRO" and  mes != "FEVEREIRO" and mes != "MARÇO" and  mes != "ABRIL" and  mes != "MAIO" and  mes != "JUNHO" and  mes != "JULHO" and  mes != "AGOSTO" and  mes != "SETEMBRO" and  mes != "OUTUBRO" and  mes != "NOVEMBRO" and  mes != "DEZEMBRO":
     print("mês inválido!")
-    mes_referencia = input("\nDigite o mês de referência: ").upper()
+    mes = input("\nDigite o mês de referência: ").upper()
 else:
-    dias_venda = int(input(f"\nQuantos dias de venda serão registrados em {mes_referencia}: "))
+    dias_venda = int(input(f"\nQuantos dias de venda serão registrados em {mes}: "))
 
+soma = 0
 
 for d in range(1, dias_venda+1):
 
-    valor_vendido = float(input(f"\nDigite o valor vendido do {d}° dia : "))
+    valor_vendido = float(input(f"\nDigite o valor vendido do {d}° dia : R$"))
+
+    soma = soma + valor_vendido
+
+print(f"A soma das vendas do mes de {mes} é de R${soma:.2f}")
     
 
 
