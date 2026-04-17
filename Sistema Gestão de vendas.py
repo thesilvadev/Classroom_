beaca02197cdbbc03e0044dc9ad065fc3651f46c
Nicolas <nicolashenriquesilva@gmail.com>
2026-04-17 00:36:23 -0300

    Commit final

--- Sistema Gestão de vendas.py
+++ Sistema Gestão de vendas.py
@@ -1,36 +1,91 @@
-print("\n------SISTEMA DE GESTÃO DE VENDAS MENSAIS------\n")
+start = "s"
 
-soma = 0
+while start == "s":
 
 
-nome_gestor = input("\nDigite o nome do gestor: ").capitalize()
+    print("\n------SISTEMA DE GESTÃO DE VENDAS MENSAIS------\n")
 
-mes = input("\nDigite o mês de referência: ").upper()
+    total_vendido = 0
+    maior_venda = 0
+    menor_venda = 9999999
 
-while mes != "JANEIRO" and  mes != "FEVEREIRO" and mes != "MARÇO" and  mes != "ABRIL" and  mes != "MAIO" and  mes != "JUNHO" and  mes != "JULHO" and  mes != "AGOSTO" and  mes != "SETEMBRO" and  mes != "OUTUBRO" and  mes != "NOVEMBRO" and  mes != "DEZEMBRO":
-    print("mês inválido!")
+
+    nome_gestor = input("\nDigite o nome do gestor: ").capitalize()
+
     mes = input("\nDigite o mês de referência: ").upper()
-else:
-    dias_venda = int(input(f"\nQuantos dias de venda serão registrados em {mes}: "))
 
-while dias_venda >= 32:
+    while mes != "JANEIRO" and  mes != "FEVEREIRO" and mes != "MARÇO" and  mes != "ABRIL" and  mes != "MAIO" and  mes != "JUNHO" and  mes != "JULHO" and  mes != "AGOSTO" and  mes != "SETEMBRO" and  mes != "OUTUBRO" and  mes != "NOVEMBRO" and  mes != "DEZEMBRO":
+        print("mês inválido!")
+        mes = input("\nDigite o mês de referência: ").upper()
+    else:
+        dias_venda = int(input(f"\nQuantos dias de venda serão registrados em {mes}: "))
 
-    dias_venda = int(input(f"\nPor favor, digite um dia válido para o mes de {mes}: "))
+    while dias_venda >= 32: 
 
-else:
-    for d in range(1, dias_venda+1):
+        dias_venda = int(input(f"\nPor favor, digite um dia válido para o mes de {mes}: "))
 
-        valor_vendido = float(input(f"\nDigite o valor vendido do {d}° dia : R$"))
+    else:
+        for dia in range(1, dias_venda+1):
 
-        soma = soma + valor_vendido
+            valor = float(input(f"\nDigite o valor vendido do {dia}° dia : R$"))              
+
+            total_vendido = total_vendido + valor
+        
+            if valor > maior_venda:
+                maior_venda = valor
+
+            if valor < menor_venda:
+                menor_venda = valor
+        
 
 
-media = soma / d
+
+    media = total_vendido / dias_venda
+
+
+    sem_venda = 31 - dias_venda
+
+
+    if total_vendido <10000:
+        meta_mensal = "Não atingida"
+    else:
+        meta_mensal = "Atingida!"
+
+    if total_vendido < 8500:
+        aproveitamento = "Aproveitamento Regular"
+    elif total_vendido >= 8500 and total_vendido <= 10000:
+        aproveitamento = "Aproveitamento Bom"
+    elif total_vendido >10000 and total_vendido <12500:
+        aproveitamento = "Aproveitamento Ótimo"
+    elif total_vendido > 12500:
+        aproveitamento = "Aproveitamento Excelente"
+
+
+    print(f"\n========== RELATÓRIO DE VENDAS ==========\n")
+    print(f"Gestor: {nome_gestor}\n")
+    print(f"Mês: {mes}\n")
+    print(f"\nDias com vendas neste mês: {dias_venda} dias")
+    print(f"\nTotal vendido no período: R$ {total_vendido:.2f}")
+    print(f"\nMédia de vendas diárias: R$ {media:.2f}")
+    print(f"\nMeta mensal: {meta_mensal}")
+    print(f"\nMaior venda do período: R$ {maior_venda:.2f}")
+    print(f"\nMenor venda do período: R$ {menor_venda:.2f}")
+    print(f"\nDias sem venda: {sem_venda} dias")
+    print(f"\nAproveitamento meta mensal: {aproveitamento}")
+    print(f"\n========== FIM DO RELATÓRIO ==========\n")
+
+    start = input("\n\nDeseja refazer o relatório? (s/n)\nR: ")
+
+    if start != "s":
+        print("FIM DO PROGRAMA")
 
 
 
 
-print(f"\nA soma das vendas do mes de {mes} é de R$ {soma:.2f}")
+
+
+
+
     
 
 
